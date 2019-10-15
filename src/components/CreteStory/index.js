7cbddf450ec78f09fd6dfb14692e536350a91a4d
Christian TaLoche <meutrix@gmail.com>
2019-10-15 13:56:18 +0200

    Mise en place dede la page Create Story

---
+++ src/components/CreteStory/index.js
@@ -0,0 +1,13 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+
+
+// == Composant
+const CreateStory = () => (
+  <div>Rejoindre l'aventure</div>
+);
+
+// == Export
+export default CreateStory;
