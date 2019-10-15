95ede02fe398d03138978293aaadd24e11bb5dc7
Christian TaLoche <meutrix@gmail.com>
2019-10-15 22:34:36 +0200

    intrégration faite sauf boutons validation

--- src/components/CreateStory/index.js
+++ src/components/CreateStory/index.js
@@ -1,75 +1,68 @@
 // == Import : npm
 import React from 'react';
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import {
+  Form,
+  Button,
+  Container,
+  ButtonToolbar,
+}
+  from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
+import { FiUpload } from 'react-icons/fi';
 
 // == Import : local
-
+import './createstory.scss';
 
 // == Composant
 const CreateStory = () => (
-  <Form>
-    <Form.Group controlId="formGroupEmail">
-      <Form.Label>Titre de votre histoire</Form.Label>
-      <Form.Control type="email" placeholder="Titre de votre histoire" />
-    </Form.Group>
-    <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="input-group-text" id="inputGroupFileAddon01">
-          Uploader
-        </span>
-      </div>
-      <div className="custom-file">
-        <input
-          type="file"
-          className="custom-file-input"
-          id="inputGroupFile01"
-          aria-describedby="inputGroupFileAddon01"
-        />
-        <label className="custom-file-label" htmlFor="inputGroupFile01">
-        Sélectionnez l'image de couverture de votre histoire
-        </label>
-      </div>
-    </div>
-    <Form.Group controlId="exampleForm.ControlTextarea1">
-      <Form.Control as="textarea" rows="3" placeholder="Ecrivez une description de votre histoire..." />
-    </Form.Group>
-    <Form.Group controlId="Form.ControlCatSelect">
-      <Form.Label>Catégories d'histoire</Form.Label>
-      <Form.Control as="select">
-        <option>Fantastique</option>
-        <option>Science-fiction</option>
-        <option>Contes</option>
-        <option>Bit-lit</option>
-        <option>Contemporain</option>
-      </Form.Control>
-    </Form.Group>
-    <Row>
-      <Col>
-        <Form.Control placeholder="First name" />
-      </Col>
-      <Col>
-        <Form.Control placeholder="Last name" />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <Button variant="primary" type="submit">
-          Supprimer
-        </Button>
-      </Col>
-      <Col>
-        <Button variant="primary" type="submit">
-          Modifier
-        </Button>
-      </Col>
-      <Col>
-        <Button variant="primary" type="submit">
-          Sauvegarder
-        </Button>
-      </Col>
-    </Row>
-  </Form>
-
+  <Jumbotron fluid>
+    <Container>
+      <Form>
+        <Form.Group controlId="formTitleStory">
+          <Form.Control type="texte" placeholder="Titre de votre histoire" />
+        </Form.Group>
+        <Form.Group controlId="ControlTextareaDescription">
+          <Form.Control as="textarea" rows="10" placeholder="Ecrivez une description de votre histoire..." />
+        </Form.Group>
+        <Form.Group controlId="Form.ControlCatSelect">
+          <Form.Control as="select">
+            <option>Choisissez la catégorie de votre histoire...</option>
+            <option>Fantastique</option>
+            <option>Science-fiction</option>
+            <option>Contes</option>
+            <option>Bit-lit</option>
+            <option>Contemporain</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Row className="upload">
+          <Form.Group>
+            <Button variant="dark"><FiUpload /> Uploadez l'image de couverture de l'histoire</Button>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label classame="publication">Statut</Form.Label>
+            <BootstrapSwitchButton
+              onlabel='Publié'
+              offlabel='Brouillon'
+              checked={false}
+              width={150}
+            />
+          </Form.Group>
+        </Form.Row>
+        <ButtonToolbar>
+          <Button className="custom-mobile" variant="outline-danger" size="sm">
+            Supprimer
+          </Button>
+          <Button className="custom-mobile" variant="secondary" size="sm">
+            Modifier
+          </Button>
+          <Button className="custom-mobile" variant="info" size="sm">
+            Sauvegarder
+          </Button>
+        </ButtonToolbar>
+      </Form>
+    </Container>
+  </Jumbotron>
 );
 
 // == Export
