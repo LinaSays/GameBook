0fc953efb253b353bad7ad1d7cdf947c986633bc
LinaSays <moskvina.alina10@gmail.com>
2019-10-16 18:09:03 +0200

    profile composant

---
+++ src/components/Profile/Info.js
@@ -0,0 +1,16 @@
+// == Import : npm
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// == Import : local
+
+// == Composant
+const Info = () => (
+  <>
+  Text
+  </>
+);
+
+
+// == Export
+export default Info;
