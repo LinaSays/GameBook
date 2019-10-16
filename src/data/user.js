bb5681bcd1fa13b3387adba375aeec8f57a015bc
LinaSays <moskvina.alina10@gmail.com>
2019-10-16 14:51:28 +0200

    add user data

---
+++ src/data/user.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'Victor Hugo',
+  email: 'meutrix@gmail.com',
+  password: 'vivelafrance',
+  avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Victor_Hugo_001.jpg/1200px-Victor_Hugo_001.jpg',
+};
