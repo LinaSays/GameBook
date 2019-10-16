291ea05a11a27a0065fb02c6376f51581a896fb8
Baptiste P <parville.baptiste@gmail.com>
2019-10-16 14:04:27 +0200

    Setting up the bookstore Flippy (web)

--- src/components/Categories/category.js
+++ src/components/Categories/category.js
@@ -2,8 +2,7 @@
  * Import : npm
  */
 import React from 'react';
-// import { Card } from 'semantic-ui-react';
-// import { FaRegArrowAltCircleDown } from 'react-icons/fa';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 /*
  * Import : local
@@ -14,13 +13,36 @@ import './category.scss';
  * Components
  */
 const Category = () => (
-  <div>
-    <img
-      src="https://jeromeobiols.com/wordpress/wp-content/uploads/voie-lactee-montagne.jpg"
-      alt=""
-      className="card-image-category"
-    />
-  </div>
+  <Flippy
+    flipOnHover={false} // default false
+    flipOnClick // default false
+    flipDirection="horizontal" // horizontal or vertical
+    style={{ marginTop: '1em' }}
+    // ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+    // if you pass isFlipped prop component will be controlled component.
+    // and other props, which will go to div }
+  >
+    <FrontSide style={{ padding: '0' }}>
+      <div className="card">
+        <img
+          src="https://jeromeobiols.com/wordpress/wp-content/uploads/voie-lactee-montagne.jpg"
+          alt=""
+          className="card-image-category"
+        />
+        <h2 className="card-text-front">Categorie - <span className="card-text-front-author">auteur</span></h2>
+      </div>
+    </FrontSide>
+    <BackSide style={{ padding: '0' }}>
+      <div className="card">
+        <img
+          src="https://jeromeobiols.com/wordpress/wp-content/uploads/voie-lactee-montagne.jpg"
+          alt=""
+          className="card-image-category"
+        />
+        <h2 className="card-text-back">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi inventore iusto tenetur, laudantium nesciunt optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptates, eaque impedit non, ad rerum consectetur id nam accusamus est, magnam mollitia. Quae nobis sed iste illo quis vitae eligendi?</h2>
+      </div>
+    </BackSide>
+  </Flippy>
 );
 
 /*
