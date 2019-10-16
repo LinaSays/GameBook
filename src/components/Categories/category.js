f189265392837fbb704ff2c39ddf1427de9d7736
Baptiste P <parville.baptiste@gmail.com>
2019-10-16 12:29:17 +0200

    Setting up the banner and category images

---
+++ src/components/Categories/category.js
@@ -0,0 +1,29 @@
+/*
+ * Import : npm
+ */
+import React from 'react';
+// import { Card } from 'semantic-ui-react';
+// import { FaRegArrowAltCircleDown } from 'react-icons/fa';
+
+/*
+ * Import : local
+ */
+import './category.scss';
+
+/*
+ * Components
+ */
+const Category = () => (
+  <div>
+    <img
+      src="https://jeromeobiols.com/wordpress/wp-content/uploads/voie-lactee-montagne.jpg"
+      alt=""
+      className="card-image-category"
+    />
+  </div>
+);
+
+/*
+ * Export
+ */
+export default Category;
---
+++ src/components/Categories/index.js
@@ -0,0 +1,42 @@
+/*
+ * Import : npm
+ */
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+/*
+ * Import : local
+ */
+import Category from './category';
+import './categories.scss';
+
+/*
+ * Components
+ */
+const Categories = () => (
+  <div className="categories">
+    <img
+      src="https://adwebdesign.fr/wp-content/grand-media/image/banniere01.2.gif"
+      alt="Bannière"
+      className="image"
+      width="100%"
+    />
+    <div className="cards-group">
+      <Row>
+        <Col><Category /></Col>
+        <Col><Category /></Col>
+        <Col><Category /></Col>
+      </Row>
+      <Row>
+        <Col><Category /></Col>
+        <Col><Category /></Col>
+        <Col><Category /></Col>
+      </Row>
+    </div>
+  </div>
+);
+
+/*
+ * Export
+ */
+export default Categories;
