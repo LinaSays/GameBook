63da34e7c355797cfe5ae41823acb803c0419a0b
ismael2m <ismael.mmadi11@gmail.com>
2019-10-17 20:51:04 +0200

    Home not connected responsive

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -8,8 +8,8 @@ import './home.scss';
 // == Composant
 const Card = () => (
   <div className="card">
-    <p>CARD</p>
-    <h2 className="card-title">Lorem, ipsum dolor.</h2>
+    
+    <h2 className="card-title">Titre de l'histoire</h2>
     <a href="" className="card-lin">Commencer l'histoire ></a>
   </div>
 );
--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -11,9 +11,17 @@ import Navigation from 'src/components/Navigation';
 const Intro = () => (
   <div className="intro container">
     <Navigation />
-    <h1 className="intro-title">L'aventure dont vous êtes le héros</h1>
-    <p className="intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint consequuntur unde, itaque alias at quibusdam accusantium, commodi temporibus, fugit corporis assumenda laborum nisi officia nam eius sapiente saepe natus tempora quo explicabo? At dignissimos debitis quis nesciunt tenetur laborum aspernatur?</p>
-    <button className="intro-button" type="button">Commencez une histoire</button>
+    <div className="intro-content">
+      <div className="intro-left">
+        <h1 className="intro-title">L'aventure dont vous êtes le héros!</h1>
+        <p className="intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint consequuntur unde, itaque alias at quibusdam accusantium, commodi temporibus, fugit corporis assumenda laborum nisi officia nam eius sapiente saepe natus tempora quo explicabo? At dignissimos debitis quis nesciunt tenetur laborum aspernatur?</p>
+        <button className="intro-button" type="button">Commencez une histoire</button>
+      </div>
+      <div className="intro-img">
+        <img className="intro-img--image" src="http://preview.uxtheme.website/xpider-preview/xpider/img/devices-1.png" alt="" />
+      </div>
+
+    </div>
   </div>
 
 );
