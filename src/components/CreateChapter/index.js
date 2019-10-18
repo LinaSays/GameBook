fcca4f356184aa9989b057591ad741a4d32f8023
Christian TaLoche <meutrix@gmail.com>
2019-10-19 01:40:15 +0200

    Mise en place Create Chapter avec Reactstrap

--- src/components/CreateChapter/index.js
+++ src/components/CreateChapter/index.js
@@ -1,118 +1,79 @@
 // == Import : npm
 import React from 'react';
 import {
-  Form,
-  Button,
-  Container,
-  ButtonToolbar,
-  Row,
-  Col,
+  Jumbotron, Container, Button, Form, FormGroup, Label,
+  Input, FormFeedback, FormText, CustomInput, Breadcrumb, BreadcrumbItem,
 }
-  from 'react-bootstrap';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Pagination from 'react-bootstrap/Pagination';
-import { FiUpload, FiPenTool, FiGitMerge } from 'react-icons/fi';
-import { IoMdTrash } from 'react-icons/io';
+  from 'reactstrap';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
+import { FiTrash, FiEdit3 } from 'react-icons/fi';
+import PropTypes from 'prop-types';
 
 // == Import : local
 import './createchapter.scss';
 
+
 // == Composant
-let active = 2;
-let items = [];
-for (let number = 1; number <= 5; number++) {
-  items.push(
-    <Pagination.Item key={number} active={number === active}>
-      {number}
-    </Pagination.Item>,
-  );
-}
-
-
 const CreateChapter = () => (
   <Jumbotron fluid>
-    <Container>
-      <Row className="views-icon">
-
-          <Button variant="dark">
-            <FiPenTool />
-          </Button>
-
-
-          <Button variant="dark">
-            <FiGitMerge />
-          </Button>
-
-      </Row>
-      <Pagination>{items}</Pagination>
+    <h1 className="title">L'aventure démarre enfin...</h1>
+    <p className="lead">révélez votre talent d'écrivain et prenez en main le destin de vos lecteurs</p>
+    <Container fluid>
+      <div>
+        <Breadcrumb tag="nav" listTag="div">
+          <BreadcrumbItem tag="a" href="#">Accueil</BreadcrumbItem>
+          <BreadcrumbItem tag="a" href="#">Mon profil</BreadcrumbItem>
+          <BreadcrumbItem tag="a" href="#">Créer une histoire</BreadcrumbItem>
+          <BreadcrumbItem active tag="span">Chapitres</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
       <Form>
-        <Form.Group controlId="formTitleStory">
-          <Form.Control type="texte" placeholder="Résumé la situation de ce chapitre..." />
-        </Form.Group>
-        <Form.Group controlId="ControlTextareaDescription">
-          <Form.Control as="textarea" rows="10" placeholder="Ecrivez le texte de ce chapitre..." />
-        </Form.Group>
-        <Form.Row className="upload">
-          <Form.Group>
-            <Button variant="dark"><FiUpload /> Uploadez l'image de fond pour ce chapitre </Button>
-          </Form.Group>
-          <Form.Group>
-            <Button variant="secondary">Choisir une couleur de fond</Button>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group controlId="formControlChoiceNumberSelect">
-            <Form.Control as="select">
-              <option>Nombre de choix pour ce chapitre...</option>
-              <option>0</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formTextChoice">
-            <Form.Control placeholder="Ecrivez le texte de ce choix..." />
-          </Form.Group>
-
-          <Form.Group controlId="formControlChoiceNumberSelect">
-            <Form.Control as="select">
-              <option>Rattaché au chapitre...</option>
-              <option>0</option>
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formTextChoice">
-            <Form.Control placeholder="Ecrivez le texte de ce choix..." />
-          </Form.Group>
-          <Form.Group controlId="formControlChoiceNumberSelect">
-            <Form.Control as="select">
-              <option>Rattaché au chapitre...</option>
-              <option>0</option>
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-        <Button className="trash-icon" variant="danger">
-          <IoMdTrash />
-        </Button>
-        <ButtonToolbar>
-          <Button className="custom-button-class" variant="secondary" size="sm">
-            Modifier
-          </Button>
-          <Button className="custom-button-class" variant="info" size="sm">
-            Sauvegarder
-          </Button>
-          <Button className="custom-button-class-delete" variant="outline-danger" size="sm">
-            Supprimer
-          </Button>
-        </ButtonToolbar>
+        <FormGroup>
+          <h4>Prenez ici, le temps de configurer de votre histoire</h4>
+        </FormGroup>
+        
       </Form>
     </Container>
   </Jumbotron>
 );
-
 // == Export
 export default CreateChapter;
+Jumbotron.propTypes = {
+  fluid: PropTypes.bool,
+};
+Input.propTypes = {
+  type: PropTypes.string,
+  valid: PropTypes.bool,
+  invalid: PropTypes.bool,
+  className: PropTypes.string,
+};
+CustomInput.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  type: PropTypes.string.isRequired, // radio, checkbox, select, range, switch, file.
+  label: PropTypes.string, // used for checkbox and radios
+  valid: PropTypes.bool,
+  invalid: PropTypes.bool,
+};
+FormFeedback.propTypes = {
+  valid: PropTypes.bool,
+};
+Button.propTypes = {
+  active: PropTypes.bool,
+  'aria-label': PropTypes.string,
+  block: PropTypes.bool,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
+  outline: PropTypes.bool,
+};
+Container.propTypes = {
+  fluid: PropTypes.bool,
+};
+Breadcrumb.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  listTag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  'aria-label': PropTypes.string,
+};
+BreadcrumbItem.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  active: PropTypes.bool,
+};
