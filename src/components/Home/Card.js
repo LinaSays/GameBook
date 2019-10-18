9553d2fa0d171d1e79bbd3ecffc17e84e5c84929
ismael2m <ismael.mmadi11@gmail.com>
2019-10-18 15:46:23 +0200

    title and link on card added

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -8,12 +8,13 @@ import './home.scss';
 // == Composant
 const Card = () => (
   <div className="card container">
+    <img className="card-img" src="https://wallpaperplay.com/walls/full/a/5/3/102151.jpg" alt="" />
     <div className="card-text-content">
-      <h2 className="card-title">Titre de l'histoire</h2>
+      <h2 className="card-title">La Légende de la marmotte enchanté</h2>
     <a href="" className="card-link">Commencer l'histoire ></a>
     </div>
     
-    <img className="card-img" src="https://images.ctfassets.net/tqv8zqwdfnf7/4vRsDBiuxyKaywwuSyeSmk/721be5aca854399fbed7bba40524a334/Marmotta_desk.jpg?fm=jpg&fl=progressive" alt="" />
+    
   </div>
 );
 
