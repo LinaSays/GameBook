8ebd2417ee9fd3a53d413b651972fa2dc589859a
ismael2m <ismael.mmadi11@gmail.com>
2019-10-18 14:54:21 +0200

    card size responsive done

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -7,10 +7,13 @@ import './home.scss';
 
 // == Composant
 const Card = () => (
-  <div className="card">
+  <div className="card container">
+    <div className="card-text-content">
+      <h2 className="card-title">Titre de l'histoire</h2>
+    <a href="" className="card-link">Commencer l'histoire ></a>
+    </div>
     
-    <h2 className="card-title">Titre de l'histoire</h2>
-    <a href="" className="card-lin">Commencer l'histoire ></a>
+    <img className="card-img" src="https://images.ctfassets.net/tqv8zqwdfnf7/4vRsDBiuxyKaywwuSyeSmk/721be5aca854399fbed7bba40524a334/Marmotta_desk.jpg?fm=jpg&fl=progressive" alt="" />
   </div>
 );
 
