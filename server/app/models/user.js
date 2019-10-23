c6dc97d1a5da755a2981a92edb0651a0993a5fe6
LinaSays <moskvina.alina10@gmail.com>
2019-10-23 09:15:58 +0200

    delete create chapter

---
+++ server/app/models/user.js
@@ -0,0 +1,14 @@
+https://www.codementor.io/teodeleanu/how-to-use-sequelize-orm-in-your-express-application-u5d78po6f
+
+https://www.codementor.io/mirko0/how-to-use-sequelize-with-node-and-express-i24l67cuz
+
+https://numa-bord.com/miniblog/nodejs-exemples-dutilisation-avec-lorm-sequelize/
+
+https://sequelize.org/master/manual/getting-started.html
+
+https://sequelize.org/master/manual/associations.html
+
+https://sequelize.org/master/manual/associations.html#creating-with-associations
+
+https://sequelize.org/master/manual/models-definition.html
+
