5096222577bf62d7d32fd427544490cdcb91692b
ismael2m <ismael.mmadi11@gmail.com>
2019-10-28 22:12:51 +0100

    add scrollmagic dependancies

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -2,23 +2,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import * as ScrollMagic from 'scrollmagic';
+import { TweenMax, TimelineMax } from 'gsap';
+import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
+
 
 // == Import : local
 import './home.scss';
 
+// == Plugins
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
 // == Composant
 const Card = ({
   id, title, description, image,
-}) => (
-  <div id={`card-${id}`} className="card-story container">
-    <img className="card-story-img" src={image} alt="" />
-    <div className="card-story-text-content">
-      <h2 className="card-story-title">{title}</h2>
-      <p className="card-story-descrip">{description}</p>
-      <a href="" className={`card-story-link link-${id}`}>Commencer l'histoire ></a>
+}) => {
+  console.log('coucou');
+  return (
+    <div id={`card-${id}`} className="card-story container">
+      <img className="card-story-img" src={image} alt="" />
+      <div className="card-story-text-content">
+        <h2 className="card-story-title">{title}</h2>
+        <p className="card-story-descrip">{description}</p>
+        <a href="" className={`card-story-link link-${id}`}>Commencer l'histoire ></a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Card.propTypes = {
   id: PropTypes.number.isRequired,
