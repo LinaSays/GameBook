e20e9ff95c312e3e40c5039b0bb9e3126c902b40
LinaSays <moskvina.alina10@gmail.com>
2019-10-29 15:00:01 +0100

    minor changes 2

--- src/containers/CreateStory/index.js
+++ src/containers/CreateStory/index.js
@@ -6,6 +6,7 @@ import createStory from 'src/components/CreateStory';
 
 // Action Creators
 import { changeInput } from 'src/store/reducer/createStory';
+import { getCategory } from 'src/store/reducer/home';
 
 /* === State (données) === */
 const mapStateToProps = (state) => ({
@@ -15,12 +16,17 @@ const mapStateToProps = (state) => ({
   text: state.createStory.text,
   choice: state.createStory.choice,
   choice2: state.createStory.choice2,
+  category: state.home.category,
 });
 
 /* === Actions === */
 const mapDispatchToProps = (dispatch) => ({
   changeValue: (name, value) => {
     const action = changeInput(name, value);
+    dispatch(action);
+  },
+  getCategory: () => {
+    const action = getCategory();
     dispatch(action);
   },
 });
