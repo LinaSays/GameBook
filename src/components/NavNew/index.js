9270caa3c1d4dd5876067109899a1b71cff5cc6e
ismael2m <ismael.mmadi11@gmail.com>
2019-10-29 15:22:02 +0100

    nav managed with hooks

--- src/components/NavNew/index.js
+++ src/components/NavNew/index.js
@@ -1,39 +1,48 @@
-
 // == Import : npm
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 // == Import : local
 import './navnew.scss';
-import Burger from 'src/components/BurgerMenu';
-
 
 // == Composant
-const NavNew = ({
-  isOpen, MenuView, isConnected, LinkClic, Link,
-}) => {
-//   const SeeMenu = (e) => MenuView(e);
-//   const clickLink = (e) => LinkClic(e);
+const NavNew = ({ isConnected }) => {
 
-  //   const visibleOrnot = classNames('nav-hidden', { 'nav-visible': isOpen });
+  // hook to managed a local state
+  const [isOpen, SeeMenu] = useState(false);
+
+  // Remove scrollbar in DOM when bur menu is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = 'hidden';
+    }
+    else {
+      document.body.style.overflowY = 'visible';
+    }
+  });
+
+  // All variables who generate a classname with classNames library
+  const visibleOrnot = classNames('nav-hidden', { 'nav-visible': isOpen });
   const navContainer = 'container-fluid nav-container';
-  //   const fixOrNot = classNames(navContainer, { 'container-fluid nav-container-fixed': isOpen });
-  //   const stickyOrNOt = classNames('nav container', { 'nav container': Link !== 'accueil' });
-  //   const colorLogo = classNames('text-white', { 'text-dark': isOpen });
+  const openOrNot = classNames('nav-icon1', { open: isOpen });
+  const whiteOrNot = classNames('is-close', { 'is-open': isOpen });
+  const fixOrNot = classNames(navContainer, { 'container-fluid nav-container-fixed': isOpen });
+  const colorLogo = classNames('nav-logo text-white', { 'text-dark': isOpen });
 
+  // Remove cookie
   const removeCookie = () => {
     document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     document.location.href = '/';
   };
   return (
-    <div className={navContainer}>
+    <div className={fixOrNot}>
       {
         isConnected ? (
           <nav className="nav container">
             <div className="nav-group">
-              <NavLink to="/accueil" className="text-dark" id="gamebook">LOGO</NavLink>
+              <NavLink to="/" className={colorLogo} onClick={() => SeeMenu(!isOpen)}>LOGO</NavLink>
               <NavLink to="/profile" className="nav-group-link text-dark font-weight-bold">Mon compte</NavLink>
               <NavLink to="/concept" className="nav-group-link text-dark font-weight-bold">Catégories</NavLink>
               <NavLink to="/team" className="nav-group-link text-dark font-weight-bold">A propos</NavLink>
@@ -42,39 +51,43 @@ const NavNew = ({
             <div className="button-group">
               <NavLink to="/signin"><button type="button" onClick={removeCookie}>Se déconnecter</button></NavLink>
             </div>
-
-            <Burger isOpen={isOpen} />
-            {/* <div className={visibleOrnot}>
-              <NavLink to="/" className="nav-group-link-bis" >Accueil</NavLink>
-              <NavLink to="/concept" className="nav-group-link-bis" >Mon compte</NavLink>
-              <NavLink to="/team" className="nav-group-link-bis" >Catégories</NavLink>
-              <NavLink to="/signup" className="nav-group-link-bis" >Contact</NavLink>
-              <NavLink to="/signin" className="nav-group-link-bis" >Se déconnecter</NavLink>
-            </div> */}
+            <div className={openOrNot} onClick={() => SeeMenu(!isOpen)}>
+              <span className={whiteOrNot} />
+              <span className={whiteOrNot} />
+              <span className={whiteOrNot} />
+            </div>
+            <div className={visibleOrnot}>
+              <NavLink to="/" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Accueil</NavLink>
+              <NavLink to="/profile" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Mon compte</NavLink>
+              <NavLink to="/categories" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Catégories</NavLink>
+              <NavLink to="/contact" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Contact</NavLink>
+              <NavLink to="/" className="nav-group-link-bis" onClick={removeCookie}>Se déconnecter</NavLink>
+            </div>
           </nav>
-
         ) : (
-
-
           <nav className="nav container">
             <div className="nav-group">
-              <NavLink to="/" className="nav-logo" name="accueil" id="gamebook"><span>GAMEBook</span></NavLink>
-              <NavLink to="/" className="nav-group-link" name="accueil">Accueil</NavLink>
-              <NavLink to="/team" className="nav-group-link" name="team">La team</NavLink>
-              <NavLink to="/contact" className="nav-group-link" name="contact">Contact</NavLink>
+              <NavLink to="/" className={colorLogo} onClick={() => SeeMenu(!isOpen)}><span>GAMEBook</span></NavLink>
+              <NavLink to="/" className="nav-group-link">Accueil</NavLink>
+              <NavLink to="/team" className="nav-group-link">La team</NavLink>
+              <NavLink to="/contact" className="nav-group-link">Contact</NavLink>
             </div>
             <div className="button-group">
               <NavLink to="/signup"><button type="button" className=" button-group-link mr-3">S'inscrire</button></NavLink>
               <NavLink to="/signin"><button type="button" className="button-group-link">Se connecter</button></NavLink>
             </div>
-            <Burger isOpen={isOpen} />
-            {/* <div className={visibleOrnot}>
-              <NavLink to="/" className="nav-group-link-bis" name="accueil" >Accueil</NavLink>
-              <NavLink to="/team" className="nav-group-link-bis" name="team" >La team</NavLink>
-              <NavLink to="/contact" className="nav-group-link-bis" name="contact" >Contact</NavLink>
-              <NavLink to="/signup" className="nav-group-link-bis" >S'inscrire</NavLink>
-              <NavLink to="/signin" className="nav-group-link-bis" >Se connecter</NavLink>
-            </div> */}
+            <div className={openOrNot} onClick={() => SeeMenu(!isOpen)}>
+              <span className={whiteOrNot} />
+              <span className={whiteOrNot} />
+              <span className={whiteOrNot} />
+            </div>
+            <div className={visibleOrnot}>
+              <NavLink to="/" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Accueil</NavLink>
+              <NavLink to="/team" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>La team</NavLink>
+              <NavLink to="/contact" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Contact</NavLink>
+              <NavLink to="/signup" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>S'inscrire</NavLink>
+              <NavLink to="/signin" className="nav-group-link-bis" onClick={() => SeeMenu(!isOpen)}>Se connecter</NavLink>
+            </div>
           </nav>
         )
     }
@@ -83,11 +96,9 @@ const NavNew = ({
 };
 
 // == Validation props
-// NavNew.propTypes = {
-//   isOpen: PropTypes.bool.isRequired,
-//   MenuView: PropTypes.func.isRequired,
-//   isConnected: PropTypes.bool.isRequired,
-// };
+NavNew.propTypes = {
+  isConnected: PropTypes.bool.isRequired,
+};
 
 // == Export
 export default NavNew;
