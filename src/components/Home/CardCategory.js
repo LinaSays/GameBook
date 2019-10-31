d94e9d18cbf8ed9fb4b10d0f506275bcd4b824c2
ismael2m <ismael.mmadi11@gmail.com>
2019-10-31 16:58:49 +0100

    console bug fixed

--- src/components/Home/CardCategory.js
+++ src/components/Home/CardCategory.js
@@ -2,9 +2,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import * as ScrollMagic from 'scrollmagic';
-import { TweenMax, TimelineMax } from 'gsap';
-import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
 
 // == Import : local
 import './cardcat.scss';
@@ -31,16 +28,14 @@ const CardCategory = ({
   });
 
   return (
-    <Link to={`categories/${id}`}>
-      <div className="card-story-category container" category_id={id} id={`card-story${id}`}>
-        <img className="card-story-category-img" src={image} alt="" />
-        <div className="card-story-category-text-content">
-          <h2 className="card-story-category-title">{name}</h2>
-          <p className="card-story-category-descrip">{description}</p>
-          <Link to={`categories/${id}`} className={`card-story-category-link cat-link-${id}`}>Commencer l'histoire ></Link>
-        </div>
+    <div className="card-story-category container" category_id={id} id={`card-story${id}`}>
+      <img className="card-story-category-img" src={image} alt="" />
+      <div className="card-story-category-text-content">
+        <h2 className="card-story-category-title">{name}</h2>
+        <p className="card-story-category-descrip">{description}</p>
+        <Link to={`categories/${id}`} className={`card-story-category-link cat-link-${id}`}>Commencer l'histoire ></Link>
       </div>
-    </Link>
+    </div>
   );
 };
 
