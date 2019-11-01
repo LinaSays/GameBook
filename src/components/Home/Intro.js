d6b74b727a26211e9f1400e730998a403f601188
ismael2m <ismael.mmadi11@gmail.com>
2019-11-01 09:44:03 +0100

    inter UI font

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -2,10 +2,9 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-
 // == Import : local
 import './home.scss';
-import NavNew from '../NavNew';
+
 
 
 // == Composant
@@ -13,7 +12,7 @@ const Intro = () => (
   <div className="intro container">
     <div className="intro-content">
       <div className="intro-left">
-        <h1 className="intro-title">L'aventure dont vous êtes le héros!</h1>
+        <h1 className="intro-title">G CL f'aventure dont vous êtes le héros!</h1>
         <p className="intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint consequuntur unde, itaque alias at quibusdam accusantium, commodi temporibus.Sint consequuntur unde, itaque alias at quibusdam accusantium, commodi temporibus</p>
         <AnchorLink offset="100" href="#card-1" className="intro-button">Commencer une histoire</AnchorLink>
       </div>
