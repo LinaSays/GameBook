68bdcd4cfc87e1704f98c0a38a92c5404122684d
ismael2m <ismael.mmadi11@gmail.com>
2019-11-03 14:57:58 +0100

    bug fixed on animation card category on home connected

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,48 +8,53 @@ import './home.scss';
 import { Link } from 'react-router-dom';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 import Intro from './Intro';
-import Card from './Card';
 
 
 // == Composant
 class Home extends React.Component {
-
-  cardRef = React.createRef(); 
+  cardRef = React.createRef();
 
   componentDidMount() {
     const { getStories, getCategory } = this.props;
     getStories();
     getCategory();
-
-    
   }
 
   componentDidUpdate() {
     const controller = new ScrollMagic.Controller();
+    const { isConnected } = this.props;
+    const card = document.querySelector('.content');
 
-    const card = this.cardRef.current.childNodes;
+    const cardArray = card.childNodes.length;
 
-    for(let i = 0; i<=card.length; i++) {
-      const tween = TweenMax.to(`#card-${i}`, 0.1, {scale: 1.067, repeat : 1, yoyo: true});
-      
-      
-      const scene = new ScrollMagic.Scene ({
-      
-          triggerElement: `#card-${i}`, 
-          duration: "450px",
-          //offset:200,
-      })
-      
-      
-      .setTween(tween)
-      .addIndicators({name: "resize"}) // add indicators (requires plugin)
-      
-      .addTo(controller)
-      
-      
-      
+    if (isConnected) {
+      for (let i = 2; i <= cardArray; i++) {
+        const tween = TweenMax.to(`#card-${i}`, 0.1, { scale: 1.067, repeat: 1, yoyo: true });
+        new ScrollMagic.Scene({
+          triggerElement: `#card-${i}`,
+          duration: '500px',
+          
+        })
+          .setTween(tween)
+          .addIndicators({ name: 'resize' }) // add indicators (requires plugin)
+
+          .addTo(controller);
       }
+    }
+    else {
+      for (let i = 1; i <= cardArray; i++) {
+        const tween = TweenMax.to(`#card-${i}`, 0.1, { scale: 1.067, repeat: 1, yoyo: true });
 
+        new ScrollMagic.Scene({
+          triggerElement: `#card-${i}`,
+          duration: '500px',
+
+        })
+          .setTween(tween)
+          .addIndicators({ name: 'resize' }) // add indicators (requires plugin)
+          .addTo(controller);
+      }
+    }
   }
 
   render() {
@@ -61,12 +66,12 @@ class Home extends React.Component {
           isConnected ? (
             <div className="content">
               {category.map((item) => (
-                <div key={item.id} ref={this.card} className="card-story-category container" category_id={item.id} id={`card-story${item.id}`}>
-                  <img className="card-story-category-img" src={item.image} alt="" />
-                  <div className="card-story-category-text-content">
-                    <h2 className={`card-story-category-title cat-title-${item.id}`}>{item.name} .</h2>
-                    <p className="card-story-category-descrip">{item.description}</p>
-                    <Link to={`categories/${item.id}`} className={`card-story-category-link cat-link-${item.id}`}>Commencer l'histoire<MdKeyboardArrowRight /></Link>
+                <div key={item.id} className="card-story container" category_id={item.id} id={`card-${item.id}`}>
+                  <img className="card-story-img" src={item.image} alt="" />
+                  <div className="card-story-text-content">
+                    <h2 className={`card-story-title title-${item.id}`}>{item.name} .</h2>
+                    <p className="card-story-descrip">{item.description}</p>
+                    <Link to={`categories/${item.id}`} className={`card-story-link link-${item.id}`}>Commencer l'histoire<MdKeyboardArrowRight /></Link>
                   </div>
                 </div>
               ))}
@@ -77,7 +82,7 @@ class Home extends React.Component {
                 <Intro isConnected={isConnected} />
                 <div className="wave" />
               </div>
-              <div ref={this.cardRef} className="content" id="histoire">
+              <div className="content" id="histoire">
                 {story.map((item) => (
                   <div key={item.id} id={`card-${item.id}`} className="card-story container">
                     <img className="card-story-img" src={item.image} alt="" />
