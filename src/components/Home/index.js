96234796449d7a1f55a7606e758e70f53881b6ae
ismael2m <ismael.mmadi11@gmail.com>
2019-11-03 13:59:06 +0100

    card animation fixed for home not connected

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,10 +13,43 @@ import Card from './Card';
 
 // == Composant
 class Home extends React.Component {
+
+  cardRef = React.createRef(); 
+
   componentDidMount() {
     const { getStories, getCategory } = this.props;
     getStories();
     getCategory();
+
+    
+  }
+
+  componentDidUpdate() {
+    const controller = new ScrollMagic.Controller();
+
+    const card = this.cardRef.current.childNodes;
+
+    for(let i = 0; i<=card.length; i++) {
+      const tween = TweenMax.to(`#card-${i}`, 0.1, {scale: 1.067, repeat : 1, yoyo: true});
+      
+      
+      const scene = new ScrollMagic.Scene ({
+      
+          triggerElement: `#card-${i}`, 
+          duration: "450px",
+          //offset:200,
+      })
+      
+      
+      .setTween(tween)
+      .addIndicators({name: "resize"}) // add indicators (requires plugin)
+      
+      .addTo(controller)
+      
+      
+      
+      }
+
   }
 
   render() {
@@ -28,7 +61,7 @@ class Home extends React.Component {
           isConnected ? (
             <div className="content">
               {category.map((item) => (
-                <div key={item.id} className="card-story-category container" category_id={item.id} id={`card-story${item.id}`}>
+                <div key={item.id} ref={this.card} className="card-story-category container" category_id={item.id} id={`card-story${item.id}`}>
                   <img className="card-story-category-img" src={item.image} alt="" />
                   <div className="card-story-category-text-content">
                     <h2 className={`card-story-category-title cat-title-${item.id}`}>{item.name} .</h2>
@@ -44,16 +77,16 @@ class Home extends React.Component {
                 <Intro isConnected={isConnected} />
                 <div className="wave" />
               </div>
-              <div className="content" id="histoire">
+              <div ref={this.cardRef} className="content" id="histoire">
                 {story.map((item) => (
-                   <div id={`card-${item.id}`} className="card-story container">
-                   <img className="card-story-img" src={item.image} alt="" />
-                   <div className="card-story-text-content">
-                     <h2 className="card-story-title">{item.title} .</h2>
-                     <p className="card-story-descrip">{item.description}</p>
-                     <Link to={`story/${item.id}`} className={`card-story-link link-${item.id}`}>Commencer l'histoire <MdKeyboardArrowRight /> </Link>
-                   </div>
-                 </div>
+                  <div key={item.id} id={`card-${item.id}`} className="card-story container">
+                    <img className="card-story-img" src={item.image} alt="" />
+                    <div className="card-story-text-content">
+                      <h2 className="card-story-title">{item.title} .</h2>
+                      <p className="card-story-descrip">{item.description}</p>
+                      <Link to={`story/${item.id}`} className={`card-story-link link-${item.id}`}>Commencer l'histoire <MdKeyboardArrowRight /> </Link>
+                    </div>
+                  </div>
                 ))}
               </div>
             </>
