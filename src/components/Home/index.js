43eb15eb4334a290c63c5eacec82456035583f08
ismael2m <ismael.mmadi11@gmail.com>
2019-11-03 12:30:30 +0100

    cards in home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 
 // == Import : local
 import './home.scss';
+import { Link } from 'react-router-dom';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 import Intro from './Intro';
 import Card from './Card';
-import CardCategory from './CardCategory';
 
 
 // == Composant
@@ -27,7 +28,14 @@ class Home extends React.Component {
           isConnected ? (
             <div className="content">
               {category.map((item) => (
-                <CardCategory key={item.id} {...item} />
+                <div key={item.id} className="card-story-category container" category_id={item.id} id={`card-story${item.id}`}>
+                  <img className="card-story-category-img" src={item.image} alt="" />
+                  <div className="card-story-category-text-content">
+                    <h2 className={`card-story-category-title cat-title-${item.id}`}>{item.name} .</h2>
+                    <p className="card-story-category-descrip">{item.description}</p>
+                    <Link to={`categories/${item.id}`} className={`card-story-category-link cat-link-${item.id}`}>Commencer l'histoire<MdKeyboardArrowRight /></Link>
+                  </div>
+                </div>
               ))}
             </div>
           ) : (
@@ -38,7 +46,14 @@ class Home extends React.Component {
               </div>
               <div className="content" id="histoire">
                 {story.map((item) => (
-                  <Card key={item.title} {...item} />
+                   <div id={`card-${item.id}`} className="card-story container">
+                   <img className="card-story-img" src={item.image} alt="" />
+                   <div className="card-story-text-content">
+                     <h2 className="card-story-title">{item.title} .</h2>
+                     <p className="card-story-descrip">{item.description}</p>
+                     <Link to={`story/${item.id}`} className={`card-story-link link-${item.id}`}>Commencer l'histoire <MdKeyboardArrowRight /> </Link>
+                   </div>
+                 </div>
                 ))}
               </div>
             </>
