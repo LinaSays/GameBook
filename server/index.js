97d4a04321d8a81cb4614bc8d769768ee0fe6fd7
LinaSays <moskvina.alina10@gmail.com>
2019-11-06 10:51:26 +0100

    test gamebook.tech

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const cookieParser = require('cookie-parser');
 // Server
 const app = module.exports = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://gamebook.tech' }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://gamebook.tech');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH, HEAD');
