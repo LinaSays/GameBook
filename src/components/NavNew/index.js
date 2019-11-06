120db1c8652867627cf76624afd7bed97592aa11
LinaSays <moskvina.alina10@gmail.com>
2019-11-06 21:29:38 +0100

    cookie domain

--- src/components/NavNew/index.js
+++ src/components/NavNew/index.js
@@ -17,7 +17,7 @@ class NavNew extends React.Component {
 
   removeCookie = () => {
     // toast.success('A très bientôt !');
-    document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=.gamebook.tech';
     document.location.href = '/';
   };
 
